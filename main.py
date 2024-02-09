5358850add33c4a8b623bba35f77b81428f74600
Michal Hradis <michal.hradis@gmail.com>
2024-02-09 18:17:05 +0100

    Added CLI arguments and tensorboard logging.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from lightning.pytorch.loggers import WandbLogger
+
 from dataset import VideoSingleFrameDataset
 import torch
 import torch.utils.data
@@ -7,26 +7,70 @@ from lightning.pytorch.callbacks import ModelCheckpoint
 import lightning as L
 
 from model import Model
+import argparse
 
-if __name__ == "__main__":
-    logger = WandbLogger(project="BP")
-    train_dataset = VideoSingleFrameDataset("data/REDS/train/train_sharp_bicubic/X4", "data/REDS/train/train_sharp")
-    val_dataset = VideoSingleFrameDataset("data/REDS/val/val_sharp_bicubic/X4", "data/REDS/val/val_sharp")
 
-    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=8, num_workers=31, shuffle=True)
-    val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=8, num_workers=32, shuffle=True)
+def parse_args():
+    parser = argparse.ArgumentParser(description="Train a model")
+    parser.add_argument("--project", type=str, help="Logger project name.")
+    parser.add_argument("--name", type=str, help="Logger experiment name.")
+    parser.add_argument("--logger-type", type=str, default="wandb", help="Logger type (wandb, tensorboard)")
 
-    model = Model()
-    logger.watch(model)
+    parser.add_argument("--train-hq", type=str, required=True, help="Path to the high quality training dataset")
+    parser.add_argument("--train-lq", type=str, required=True, help="Path to the low quality training dataset")
+    parser.add_argument("--val-hq", type=str, required=True, help="Path to the high quality validation dataset")
+    parser.add_argument("--val-lq", type=str, required=True, help="Path to the low quality validation dataset")
+    parser.add_argument("--batch-size", type=int, default=4, help="Batch size")
+    parser.add_argument("--num-workers", type=int, default=6, help="Number of workers for the dataloader")
 
-    checkpoint_callback = ModelCheckpoint(dirpath="checkpoints/srresnet", save_top_k=3, monitor="val/loss")
+    parser.add_argument("--devices", type=int, nargs="+", default=[0], help="Devices to use for training")
+
+    parser.add_argument("--epochs", type=int, default=100, help="Number of epochs to train")
+    parser.add_argument("--log-every-n-steps", type=int, default=50, help="Log every n steps")
+    parser.add_argument("--check-val-every-n-epoch", type=int, default=5, help="Check validation every n epochs")
+    parser.add_argument("--save-top-k", type=int, default=3, help="Save top k models")
+
+    parser.add_argument("--learnging-rate", type=float, default=2e-3, help="Learning rate")
+
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+
+    if args.logger_type == "wandb":
+        from lightning.pytorch.loggers import WandbLogger
+        logger = WandbLogger(project=args.project, name=args.name)
+    elif args.logger_type == "tensorboard":
+        from lightning.pytorch.loggers import TensorBoardLogger
+        logger = TensorBoardLogger(save_dir="logs", name=args.name)
+
+    train_dataset = VideoSingleFrameDataset(args.train_lq, args.train_hq)
+    val_dataset = VideoSingleFrameDataset(args.val_lq, args.val_hq)
+
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=args.batch_size, num_workers=args.num_workers, shuffle=True)
+    val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=args.batch_size, num_workers=args.num_workers, shuffle=False)
+    model = Model(lr=args.learnging_rate)
+    #logger.watch(model)
+
+    checkpoint_callback = ModelCheckpoint(
+        dirpath=f"checkpoints/{args.project}_{args.name}",
+        save_top_k=args.save_top_k, save_last=True, verbose=True,
+        monitor="val_loss", mode="min")
+
     trainer = L.Trainer(
-        max_epochs=100,
-        devices=[0, 1],
-        log_every_n_steps=50,
+        max_epochs=args.epochs,
+        devices=args.devices,
+        log_every_n_steps=args.log_every_n_steps,
         logger=logger,
-        check_val_every_n_epoch=5,
+        check_val_every_n_epoch=args.check_val_every_n_epoch,
         callbacks=[checkpoint_callback],
+        gradient_clip_val=0.5,
+        precision=16,
     )
 
     trainer.fit(model=model, train_dataloaders=train_loader, val_dataloaders=val_loader)
+
+
+if __name__ == "__main__":
+    main()
--- model.py
+++ model.py
@@ -3,13 +3,15 @@ import lightning as L
 from lightning.pytorch.loggers import WandbLogger
 from torch import nn, optim
 from model_zoo.srresnet import srresnet_x4
+import cv2
 
 torch.set_float32_matmul_precision('medium')
 
 
 class Model(L.LightningModule):
-    def __init__(self):
+    def __init__(self, lr: float = 1e-3):
         super().__init__()
+        self.lr = lr
         self.model = srresnet_x4()
         self.save_hyperparameters()
 
@@ -33,12 +35,17 @@ class Model(L.LightningModule):
         outputs = self.model(batch["LQ"])
         loss = nn.functional.mse_loss(outputs, batch["HQ"])
         self.log("val/loss", loss, prog_bar=True, logger=True)
-        self.logger.log_image("val/out", [batch["HQ"][0], outputs[0]], caption=["HQ Original", "HQ Prediction"])
+        try:
+            self.logger.log_image("val/out", [batch["HQ"][0], outputs[0]], caption=["HQ Original", "HQ Prediction"])
+        except AttributeError:
+            iteration = self.global_step
+            cv2.imwrite(f"out_{iteration}_PR.png", outputs[0].permute(1, 2, 0).cpu().numpy())
+            cv2.imwrite(f"out_{iteration}_GT.png", batch["HQ"][0].permute(1, 2, 0).cpu().numpy())
+
         return loss
 
-    
     def configure_optimizers(self):
-        optimizer = optim.Adam(self.parameters(), lr=1e-3)
+        optimizer = optim.Adam(self.parameters(), lr=self.lr)
         return optimizer
 
 
