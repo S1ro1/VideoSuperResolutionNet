95b131a6be1f5c3261cd73128b996f4939ef371c
S1ro1 <matej.sirovatka@gmail.com>
2024-02-15 15:45:33 +0000

    Improve logging, better run script

--- model.py
+++ model.py
@@ -16,17 +16,28 @@ class Model(L.LightningModule):
         self.model = srresnet_x4()
         self.save_hyperparameters()
 
-    def _compute_metrics(self, outputs: torch.Tensor, targets: torch.Tensor) -> dict[str, torch.Tensor]:
+    def _compute_metrics(self, outputs: torch.Tensor, targets: torch.Tensor, compute_loss: bool = True) -> dict[str, torch.Tensor]:
         metrics = {}
         metrics["ssim"] = structural_similarity_index_measure(
             outputs, targets)
         metrics["psnr"] = peak_signal_noise_ratio(outputs, targets)
-        metrics["loss"] = nn.functional.mse_loss(outputs, targets)
+        if compute_loss:
+            metrics["loss"] = nn.functional.mse_loss(outputs, targets)
         return metrics
 
     def _log_metrics(self, metrics: dict[str, torch.Tensor], prefix: str) -> None:
         for key, value in metrics.items():
             self.log(f"{prefix}/{key}", value, prog_bar=True, logger=True)
+
+    def _log_images(self, section: str, images: dict[str, torch.Tensor]) -> None:
+        try:
+            self.logger.log_image(
+                section, list(images.values()), caption=list(images.keys()))
+        except AttributeError:
+            iteration = self.global_step
+            for k, v in images.items():
+                cv2.imwrite(f"{section}_{iteration}_{k}.png",
+                            v.permute(1, 2, 0).cpu().numpy())
 
     def training_step(self, batch: dict[str, torch.Tensor], batch_idx: int) -> torch.Tensor:
         """training_step method of Model.
@@ -49,16 +60,15 @@ class Model(L.LightningModule):
         outputs = self.model(batch["LQ"])
         metrics = self._compute_metrics(outputs, batch["HQ"])
         self._log_metrics(metrics, "val")
-        try:
-            self.logger.log_image(
-                "val/out", [batch["HQ"][0], outputs[0]], caption=["HQ Original", "HQ Prediction"])
-        except AttributeError:
-            iteration = self.global_step
-            cv2.imwrite(f"out_{iteration}_PR.png",
-                        outputs[0].permute(1, 2, 0).cpu().numpy())
-            cv2.imwrite(f"out_{iteration}_GT.png",
-                        batch["HQ"][0].permute(1, 2, 0).cpu().numpy())
 
+        bilinear = nn.functional.interpolate(
+            batch["LQ"], scale_factor=4, mode="bilinear", align_corners=False)
+        bilinear_metrics = self._compute_metrics(
+            bilinear, batch["HQ"], compute_loss=False)
+        self._log_metrics(bilinear_metrics, "val_upsampled")
+
+        self._log_images(
+            "val/out", {"HQ": batch["HQ"][0], "HQ Prediction": outputs[0], "Bilinear": bilinear[0]})
         return metrics["loss"]
 
     def configure_optimizers(self):
--- run.sh
+++ run.sh
@@ -1,16 +1,15 @@
 CWD="/home/xsirov00/BP_code"
-
+DEBUG=${DEBUG:-0}
 
 source $CWD/.venv/bin/activate
 if [ "$DEBUG" -eq 1 ]; then
-    EXTRA_ARGS="--limit-batches 0.01 --epochs 1"
+    EXTRA_ARGS="--limit-batches 0.01 --epochs 1 --log-every-n-steps 1 --check-val-every-n-epoch 1"
 else
-    EXTRA_ARGS="--epochs 10"
+    EXTRA_ARGS="--epochs 1000 --log-every-n-steps 1 --check-val-every-n-epoch 5"
 fi
 
 python $CWD/main.py \
     --project BP --name BP_test --logger-type wandb \
     --train-hq $CWD/data/REDS/train/train_sharp --train-lq $CWD/data/REDS/train/train_sharp_bicubic/X4 \
     --val-hq $CWD/data/REDS/val/val_sharp --val-lq $CWD/data/REDS/val/val_sharp_bicubic/X4 \
-    --batch-size 8 --num-workers 32 --devices 0 --log-every-n-steps 1 \
-    --check-val-every-n-epoch 1 --save-top-k 3 $EXTRA_ARGS
+    --batch-size 8 --num-workers 32 --devices 0  --save-top-k 3 $EXTRA_ARGS
