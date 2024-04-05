9ba2105005e00c3c40d937566bc00a77f6b47e30
S1ro1 <matej.sirovatka@gmail.com>
2024-04-05 21:36:03 +0000

    Feat: use previous connection to predict new features

--- utils/arch.py
+++ utils/arch.py
@@ -243,9 +243,19 @@ class PyramidMotionCompensationFetaureFusion(nn.Module):
             use_previous (bool, optional): Unimplemented. Defaults to False.
             use_convs (bool, optional): If set to True, features are refined with convolutional layers. Defaults to True.
             use_middle_frame (bool, optional): If set to True, middle frame is used to predict the aligned features. Defaults to True.
+            use_previous (bool, optional): If set to True, previous level features are used to predict the aligned features. Defaults to False.
         """
         super(PyramidMotionCompensationFetaureFusion, self).__init__()
         self.skip_connections = nn.ModuleDict()
+        self.use_previous = use_previous
+
+        if self.use_previous:
+            self.upsample = nn.Upsample(scale_factor=2, mode="bilinear", align_corners=False)
+            self.prev_fusions = nn.ModuleDict()
+            self.prev_convs = nn.ModuleDict()
+
+        self.num_levels = len(in_channels)
+
         for level, (ic, oc) in enumerate(zip(in_channels, out_channels)):
             level_name = f"level_{level}"
             self.skip_connections[level_name] = MotionCompensationFeatureFusion(
@@ -257,6 +267,9 @@ class PyramidMotionCompensationFetaureFusion(nn.Module):
                 use_convs=use_convs,
                 use_middle_frame=use_middle_frame,
             )
+            if self.use_previous and level < self.num_levels - 1:
+                self.prev_fusions[level_name] = nn.Conv2d(2 * oc, oc, 3, 1, 1)
+                self.prev_convs[level_name] = nn.Conv2d(out_channels[level + 1], oc, 3, 1, 1)
 
     def forward(self, xs: List[torch.Tensor], of: torch.Tensor) -> List[torch.Tensor]:
         """Forward method
@@ -275,11 +288,23 @@ class PyramidMotionCompensationFetaureFusion(nn.Module):
             self.ofs.append(of_.view(b, t, c, h_, w_) / (2.0**i))
 
         out = []
-        for i, (x, of) in enumerate(zip(xs, self.ofs)):
-            level_name = f"level_{i}"
-            out.append(self.skip_connections[level_name](x, of))
 
-        return out
+        prev_features = None
+
+        for i, (x, of) in enumerate(zip(xs[::-1], self.ofs[::-1])):
+            level = self.num_levels - i - 1
+            level_name = f"level_{level}"
+            features = self.skip_connections[level_name](x, of)
+
+            if self.use_previous and prev_features is not None:
+                prev = self.prev_convs[level_name](self.upsample(prev_features))
+                features = self.prev_fusions[level_name](torch.cat([features, prev], dim=1))
+
+            prev_features = features
+
+            out.append(features)
+
+        return out[::-1]
 
 
 class UpsampleBlock(nn.Module):
@@ -312,6 +337,7 @@ class SuperResolutionUnet(smp.Unet):
         num_scales: int = 3,
         use_skip_connections: bool = True,
         use_middle_frame: bool = True,
+        use_previous: bool = False,
         *args,
         **kwargs,
     ):
@@ -326,6 +352,7 @@ class SuperResolutionUnet(smp.Unet):
             num_scales (int, optional): Num scales of the refinement convolutional layers. Defaults to 3.
             use_skip_connections (bool, optional): If set to True, feature alignment skip connections are used. Defaults to True.
             use_middle_frame (bool, optional): If set to True, middle frame is used to predict the aligned features. Defaults to True.
+            use_previous (bool, optional): If set to True, previous level features are used to predict the aligned features in the skip connection. Defaults to False.
         """
         super(SuperResolutionUnet, self).__init__(*args, **kwargs)
         self.use_skip_connections = use_skip_connections
@@ -351,6 +378,7 @@ class SuperResolutionUnet(smp.Unet):
                 center_frame_idx=1,
                 use_convs=use_convs,
                 use_middle_frame=use_middle_frame,
+                use_previous=use_previous,
             )
 
         self.segmentation_head = None
